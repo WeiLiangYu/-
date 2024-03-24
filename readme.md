adeb06ca89b084d1f9691fbfda1f966f0f6835b5
WeiLiangYu <innocence846@gmail.com>
2024-03-24 23:56:54 +0800

    添加文字雲分析

--- readme.md
+++ readme.md
@@ -1,8 +1,54 @@
-### 用於分析104職缺資訊
+# 用於分析104職缺資訊
 
 資料建立時間: 2024/03/23 <br>
 使用網站: 104 <br>
-用途: 個人用<br>
+用途: 個人用，僅供參考<br>
 步驟: <br>
     1. 使用Python爬蟲Search API抓取工作簡介，再爬取工作詳細頁面的資訊 -> data。<br>
-    2. 使用爬蟲的xlsx檔案進行資訊分析 -> img。
+    2. 使用爬蟲的xlsx檔案進行資訊分析 -> img。<br>
+
+
+## 資料工程師<br>
+### 地區:<br>
+65%在台北，其次為新北與桃園。<br>
+![資料工程師_地區](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E5%B7%A5%E4%BD%9C%E5%9C%B0%E5%8D%80.jpg?raw=true)<br><br>
+### 所需技能<br>
+主要: **軟體程式設計**、**資料庫程式設計**，<br>
+其次: **資料庫系統維護**、**軟體工程系統開發**、**資料庫軟體應用**<br>
+![資料工程師_工作技能](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E5%B7%A5%E4%BD%9C%E6%8A%80%E8%83%BD.jpg?raw=true)<br><br>
+### 擅長工具: <br>
+1.Python<br>
+2.MySQL<br>
+![資料工程師_擅長工具](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E6%93%85%E9%95%B7%E5%B7%A5%E5%85%B7.jpg?raw=true)<br><br>
+### 應用產業:<br>
+電腦軟體服務業
+![資料工程師_應用產業](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E7%94%A2%E6%A5%AD.jpg?raw=true)<br><br>
+### 科系要求: <br>
+1.資訊工程相關<br>
+2.資訊管理相關<br>
+3.數理統計相關<br>
+![資料工程師_科系要求](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB/%E8%B3%87%E6%96%99%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E7%A7%91%E7%B3%BB%E8%A6%81%E6%B1%82.jpg?raw=true)<br><br>
+
+## AI工程師<br>
+### 地區:<br>
+50%在台北，其次為新北與桃園。<br>
+![AI工程師_地區](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E5%B7%A5%E4%BD%9C%E5%9C%B0%E5%8D%80.jpg?raw=true)<br><br>
+### 所需技能<br>
+主要: **軟體程式設計**<br>
+其次: **Machine Learning**、**軟體工程系統開發**<br>
+![AI工程師_工作技能](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E5%B7%A5%E4%BD%9C%E6%8A%80%E8%83%BD.jpg?raw=true)<br><br>
+### 擅長工具: <br>
+1.Python<br>
+2.C++<br>
+![AI工程師_擅長工具](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E6%93%85%E9%95%B7%E5%B7%A5%E5%85%B7.jpg?raw=true)<br><br>
+### 應用產業:<br>
+電腦軟體服務業
+![AI工程師_應用產業](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E7%94%A2%E6%A5%AD.jpg?raw=true)<br><br>
+### 科系要求: <br>
+1.資訊工程相關<br>
+2.電機電子工程相關<br>
+3.資訊管理相關<br>
+4.數理統計相關<br>
+![AI工程師_科系要求](https://github.com/WeiLiangYu/104_AnalyzeJob/blob/main/img/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB/AI%E5%B7%A5%E7%A8%8B%E5%B8%AB_%E7%A7%91%E7%B3%BB%E8%A6%81%E6%B1%82.jpg?raw=true)<br>
+> 注: 電機電子工程相關會排在第二名，出乎了我的意料之外。<br>
+> 可能是數學科系被拆分為5個類別或更多。資料需要在做統整清理為相同。
---
+++ worldCloud.py
@@ -0,0 +1,83 @@
+import jieba
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.font_manager as fm # 中文字
+from collections import Counter
+import os
+import re
+
+
+# 設置中文字
+fontPath = 'data/微軟正黑體.ttf'
+front = fm.FontProperties(fname=fontPath)
+plt.rcParams['font.family'] = front.get_name() 
+
+# 指定辭典檔
+jieba.set_dictionary('jieba_data/dict.txt.big')
+
+# stop word
+with open(file='jieba_data/stop_words2.txt', mode='r', encoding='utf-8') as file:
+    stop_words = file.read().split('\n')
+with open(file='jieba_data/allow_words.txt', mode='r', encoding='utf-8') as file:
+    allow_words = file.read().split('\n')
+
+# 輸出
+imgFolder = 'img'
+if not os.path.exists(imgFolder):
+    os.makedirs(imgFolder)
+    print(f'{imgFolder} 已建立。')
+else:
+    print(f'{imgFolder} 已存在。')
+
+class textCloud:
+    def __init__(self, dataName):
+        # 導入資料
+        self.dataName = dataName
+        self.df = pd.read_excel(f'./data/{dataName}.xlsx') # 導入資料
+        self.dataNums = len(self.df)
+        self.dataFolder = imgFolder + '/' + dataName
+        if not os.path.exists(self.dataFolder):
+            os.makedirs(self.dataFolder)
+            print(f'{self.dataFolder} 已建立。')
+        else:
+            print(f'{self.dataFolder} 已存在。')
+    def toolData(self, targetname):
+        """"列出常用工具名稱"""
+        targets = self.df[targetname].apply(lambda x: x.split('、'))
+        targetCounts = Counter([target for sent in targets for target in sent if '不拘' not in target]) # 跌帶每個列表，再跌帶列表的list，並去除'不拘'
+        targetCounts = sorted(targetCounts.items(), key=lambda x: x[1], reverse=True)[:20] # 結果為 list(tuple)
+        targetNames = [tool[0] for tool in targetCounts]
+        with open('jieba_data/allow_words.txt', 'a', encoding='utf-8') as f:
+            for tool in targetNames:
+                f.write(tool + '\n')
+    def clean_Punctuation(self, text):
+        """"刪除標標點符號"""
+        text = re.sub(r'[^\w\s]','',text) 
+        text = text.replace('\n','').replace('\r','').replace('\t','').replace(' ','').replace('[','').replace(']','')
+        return text
+    def jobContentCloud(self, target):
+        seg_stop_words_list = []
+        seg_words_list = []
+        # 將專有名詞加入自定義詞
+        self.toolData('擅長工具')
+        for word in allow_words:
+            jieba.add_word(word)
+        # 去除標點並jieba分詞
+        [seg_words_list.extend(jieba.cut(self.clean_Punctuation(text))) for text in self.df[target]]
+        # 過濾雜訊字
+        for item in seg_words_list:
+            if item not in stop_words:
+                seg_stop_words_list.append(item)
+        # 繪製文字雲
+        seg_stop_counter = Counter(seg_stop_words_list)
+        wordcloud = WordCloud(font_path='fonts/TaipeiSansTCBeta-Regular.ttf').generate_from_frequencies(seg_stop_counter)
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        plt.savefig(f"{imgFolder}/{self.dataName}/{self.dataName}_{target}.png")
+        # plt.show()
+        # plt.close()
+if __name__ == "__main__":
+    res = textCloud('AI工程師')
+    res.jobContentCloud('工作內容')
+    res.jobContentCloud('其他條件')
